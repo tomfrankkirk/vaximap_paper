b9b2b8c9b6a79e6a266826cb317402c0d3d8040f
tomfrankkirk <tomfrankkirk@gmail.com>
2021-11-02 20:50:56 +0000

    print DB path on load

--- load_dataset/__init__.py
+++ load_dataset/__init__.py
@@ -3,6 +3,7 @@ import os.path as op
 
 path = op.join(op.dirname(__file__), 'nov_2021.h5')
 dataset = pd.read_hdf(path, key='df')
+print('Loading database', path)
 
 # Both UK and GB are used, modify in-place so all are UK
 dataset.loc[dataset['region'] == 'GB', 'region'] = 'UK'
