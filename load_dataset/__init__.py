f8443e366f65f80fba9ee8bdce3b063dafa6aab2
Bodossian, Armen, Vodafone Group <armen.bodossian@vodafone.com>
2021-12-01 09:49:31 +0000

    Adding november analysis and output datasets

--- load_dataset/__init__.py
+++ load_dataset/__init__.py
@@ -1,8 +1,9 @@
 import pandas as pd 
 import os.path as op 
 
-path = op.join(op.dirname(__file__), 'june_2021.h5')
+path = op.join(op.dirname(__file__), 'nov_2021.h5')
 dataset = pd.read_hdf(path, key='df')
+print('Loading database', path)
 
 # Both UK and GB are used, modify in-place so all are UK
 dataset.loc[dataset['region'] == 'GB', 'region'] = 'UK'
